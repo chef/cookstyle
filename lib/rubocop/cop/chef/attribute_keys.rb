6e5010d3b95f570a3ad20a91c30910dd67e0d387
Tim Smith <tsmith84@gmail.com>
2019-06-30 09:15:23 -0700

    Merge rubocop-chef into cookstyle
    This gets us some basic auto correction. I had to do some minor updates to the cops since the add_offense method has changes in the last year.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/attribute_keys.rb
@@ -0,0 +1,92 @@
+#
+# Copyright 2016, Noah Kantrowitz
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module RuboCop
+  module Cop
+    module Chef
+      # Check which style of keys are used to access node attributes.
+      #
+      # There are two supported styles: "symbols" and "strings".
+      #
+      # @example when configuration is `EnforcedStyle: symbols`
+      #
+      #   # bad
+      #   node['foo']
+      #   node["foo"]
+      #
+      #   # good
+      #   node[:foo]
+      #
+      # @example when configuration is `EnforcedStyle: strings`
+      #
+      #   # bad
+      #   node[:foo]
+      #
+      #   # good
+      #   node['foo']
+      #   node["foo"]
+      #
+      class AttributeKeys < Cop
+        include RuboCop::Cop::ConfigurableEnforcedStyle
+
+        MSG = 'Use %s to access node attributes'.freeze
+
+        def_node_matcher :node_attribute_access?, <<-PATTERN
+          (send (send _ :node) :[] _)
+        PATTERN
+
+        def_node_matcher :node_level_attribute_access?, <<-PATTERN
+          (send (send {(send _ :node) nil} {:default :role_default :env_default :normal :override :role_override :env_override :force_override :automatic}) :[] _)
+        PATTERN
+
+        def on_send(node)
+          if node_attribute_access?(node) || node_level_attribute_access?(node)
+            # node is first child for #[], need to look for the outermost parent too.
+            outer_node = node
+            while outer_node.parent && outer_node.parent.type == :send && outer_node.parent.children[1] == :[]
+              on_node_attribute_access(outer_node.children[2])
+              outer_node = outer_node.parent
+            end
+            # One last time for the outer-most access.
+            on_node_attribute_access(outer_node.children[2])
+          end
+        end
+
+        def on_node_attribute_access(node)
+          if node.type == :str
+            style_detected(:strings)
+            add_offense(node, location: :expression, message: MSG % style) if style == :symbols
+          elsif node.type == :sym
+            style_detected(:symbols)
+            add_offense(node, location: :expression, message: MSG % style) if style == :strings
+          end
+        end
+
+        def autocorrect(node)
+          lambda do |corrector|
+            key_string = node.children.first.to_s
+            key_replacement = if style == :symbols
+                                key_string.to_sym.inspect
+                              else # strings
+                                key_string.inspect
+                              end
+            corrector.replace(node.loc.expression, key_replacement)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/rubocop/cop/chef/comments_format.rb
@@ -0,0 +1,75 @@
+#
+# Copyright 2016, Tim Smith
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module RuboCop
+  module Cop
+    module Chef
+      # Checks for incorrectly formatted headers
+      #
+      # @example
+      #
+      #   # bad
+      #   Copyright 2013-2016 Chef Software, Inc.
+      #   Recipe default.rb
+      #   Attributes default.rb
+      #   License Apache2
+      #   Cookbook tomcat
+      #   Cookbook Name:: Tomcat
+      #   Attributes File:: default
+      #
+      #   # good
+      #   Copyright:: 2013-2016 Chef Software, Inc.
+      #   Recipe:: default.rb
+      #   Attributes:: default.rb
+      #   License:: Apache License, Version 2.0
+      #   Cookbook:: Tomcat
+      #
+      class CommentFormat < Cop
+        MSG = 'Properly format header comments'.freeze
+
+        def investigate(processed_source)
+          return unless processed_source.ast
+          processed_source.comments.each do |comment|
+            next unless comment.inline? # headers aren't in blocks
+
+            if invalid_comment?(comment)
+              add_offense(comment, location: comment.loc.expression, message: MSG)
+            end
+          end
+        end
+
+        private
+
+        def autocorrect(comment)
+          # Extract the type and the actual value. Strip out "Name" or "File"
+          # 'Cookbook Name' should be 'Cookbook'. Also skip a :: if present
+          match = /^# ?([A-Za-z]+)\s?(?:Name|File)?(?:::)?\s(.*)/.match(comment.text)
+          comment_type, value = match.captures
+          correct_comment = "# #{comment_type}:: #{value}"
+
+          ->(corrector) { corrector.replace(comment.loc.expression, correct_comment) }
+        end
+
+        def invalid_comment?(comment)
+          comment_types = %w(Author Cookbook Library Attribute Copyright Recipe Resource Definition License)
+          comment_types.any? do |comment_type|
+            /^#\s*#{comment_type}\s+/.match(comment.text)
+          end
+        end
+      end
+    end
+  end
+end
