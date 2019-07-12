d97de18ee94c356323a17560016d9decde6cb743
Lamont Granquist <lamont@scriptkiddie.org>
2019-07-12 11:31:59 -0700

    add Chef/NodeSet cop with autocorrect
    autocorrects to node.normal.
    
    we need a Chef/NodeNormal to warn about that, but this is a tiny bit
    of progress.
    
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

---
+++ lib/rubocop/cop/chef/node_set.rb
@@ -0,0 +1,53 @@
+#
+# Copyright:: Copyright 2001-2019, Chef Software Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module RuboCop
+  module Cop
+    module Chef
+      # The node.set method has been removed in Chef-13 and must be replaced by node.normal.
+      #
+      # Note that node.normal keeps the semantics identical, but the use of node.normal is
+      # also discouraged.
+      #
+      # @example
+      #
+      #   # bad
+      #   node.set['foo'] = true
+      #
+      #   # good
+      #   node.normal['foo'] = true
+      #
+      class NodeSet < Cop
+        MSG = 'Do not use node.set'.freeze
+
+        def_node_matcher :node_set?, <<-PATTERN
+          (send (send _ :node) :set)
+        PATTERN
+
+        def on_send(node)
+          node_set?(node) do
+            add_offense(node, location: :expression, message: MSG, severity: :error)
+          end
+        end
+
+        def autocorrect(node)
+          lambda do |corrector|
+            corrector.replace(node.loc.expression, "node.normal")
+          end
+        end
+      end
+    end
+  end
+end
