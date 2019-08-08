901ba825576878588f9e89c6e0dae021d9591407
Tim Smith <tsmith84@gmail.com>
2019-08-07 23:38:43 -0700

    Add a spec for Chef/CookbookUsesDatabags
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ spec/rubocop/cop/chef/effortless/data_bag_used_spec.rb
@@ -0,0 +1,35 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::CookbookUsesDatabags, :config do
+  subject(:cop) { described_class.new(config) }
+
+  it 'registers an offense when data_bag method is used' do
+    expect_violation(<<-RUBY)
+      data_bag_item('admins', login)
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Cookbook uses data bags, which cannot be used in the Effortless Infra pattern
+    RUBY
+  end
+
+  it 'registers an offense when data_bag_item method is used' do
+    expect_violation(<<-RUBY)
+      data_bag(data_bag_name)
+      ^^^^^^^^^^^^^^^^^^^^^^^ Cookbook uses data bags, which cannot be used in the Effortless Infra pattern
+    RUBY
+  end
+end
