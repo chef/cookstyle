169fa629cd6ad6a0de1b64f42c9c20fab8d25452
Chef Expeditor <chef-ci@chef.io>
2019-08-12 02:21:41 +0000

    Bump version to 5.1.20 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- lib/cookstyle/version.rb
+++ lib/cookstyle/version.rb
@@ -1,4 +1,4 @@
 module Cookstyle
-  VERSION = "5.1.19".freeze # rubocop: disable Style/StringLiterals
+  VERSION = "5.1.20".freeze # rubocop: disable Style/StringLiterals
   RUBOCOP_VERSION = '0.72.0'.freeze
 end
