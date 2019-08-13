145875c25f499718ec9bb40fe75146994c9dcdfa
Tim Smith <tsmith84@gmail.com>
2019-08-13 12:14:50 -0700

    Add Chef/RespondToResourceName and Chef/RespondToProvides cops
    Catch some old school gating that's floating around.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/modernize/respond_to_resource_name.rb
@@ -0,0 +1,53 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module RuboCop
+  module Cop
+    module Chef
+      # Chef 12.5 introduced the resource_name method for resources. Many cookbooks used
+      # respond_to?(:resource_name) to provide backwards compatibility with older chef-client
+      # releases. This backwards compatibility is no longer necessary.
+      #
+      # @example
+      #
+      #   # bad
+      #   resource_name :foo if respond_to?(:resource_name)
+      #
+      #   # good
+      #   resource_name :foo
+      #
+      class RespondToResourceName < Cop
+        MSG = 'respond_to?(:resource_name) in resources is no longer necessary in Chef Infra Client 12.5+'.freeze
+
+        def on_if(node)
+          if_respond_to_resource_name?(node) do
+            add_offense(node, location: :expression, message: MSG, severity: :refactor)
+          end
+        end
+
+        def_node_matcher :if_respond_to_resource_name?, <<~PATTERN
+        (if (send nil? :respond_to? ( :sym :resource_name ) ) ... )
+        PATTERN
+
+        def autocorrect(node)
+          lambda do |corrector|
+            corrector.replace(node.loc.expression, node.children[1].source)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/chef/modernize/respond_to_resource_name_spec.rb
@@ -0,0 +1,79 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::RespondToResourceName, :config do
+  subject(:cop) { described_class.new(config) }
+
+  it 'registers an offense with a HWRP that uses respond_to? with resource_name' do
+    expect_violation(<<-RUBY)
+    class Chef
+      class Resource
+        class Icinga2Apiuser < Chef::Resource
+          identity_attr :name
+
+          def initialize(name, run_context = nil)
+            super
+            @resource_name = :icinga2_apiuser if respond_to?(:resource_name)
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ respond_to?(:resource_name) in resources is no longer necessary in Chef Infra Client 12.5+
+            @provides = :icinga2_apiuser
+            @provider = Chef::Provider::Icinga2Instance
+            @action = :create
+            @allowed_actions = [:create, :delete, :nothing]
+            @name = name
+          end
+        end
+      end
+    end
+    RUBY
+  end
+
+  it 'registers an offense with a HWRP that uses respond_to? with resource_name' do
+    expect_no_violations(<<-RUBY)
+    class Chef
+      class Resource
+        class Icinga2Apiuser < Chef::Resource
+          identity_attr :name
+
+          def initialize(name, run_context = nil)
+            super
+            @resource_name = :icinga2_apiuser
+            @provides = :icinga2_apiuser
+            @provider = Chef::Provider::Icinga2Instance
+            @action = :create
+            @allowed_actions = [:create, :delete, :nothing]
+            @name = name
+          end
+        end
+      end
+    end
+    RUBY
+  end
+
+  it 'registers an offense with a LWRP that uses respond_to? with resource_name' do
+    expect_violation(<<-RUBY)
+    resource_name :icinga2_apiuser if respond_to?(:resource_name)
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ respond_to?(:resource_name) in resources is no longer necessary in Chef Infra Client 12.5+
+    RUBY
+  end
+
+  it 'does not register an offense with a LWRP that does not use resource_name' do
+    expect_no_violations(<<-RUBY)
+    resource_name :icinga2_apiuser
+    RUBY
+  end
+end
