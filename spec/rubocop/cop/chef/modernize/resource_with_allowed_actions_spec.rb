1950327a5a2985d21ce13e353830f7480575a7d1
Tim Smith <tsmith84@gmail.com>
2019-08-15 13:16:50 -0700

    Add Chef/CustomResourceWithAllowedActions
    Detect when a custom resource uses allowed_actions, which is no longer necessary.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ spec/rubocop/cop/chef/modernize/resource_with_allowed_actions_spec.rb
@@ -0,0 +1,51 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::CustomResourceWithAllowedActions, :config do
+  subject(:cop) { described_class.new(config) }
+
+  it 'registers an offense with a custom resource that uses allowed_actions' do
+    expect_violation(<<-RUBY)
+      property :something, String
+
+      allowed_actions [:create, :remove]
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Custom Resources don't need to define allowed_actions
+      action :create do
+        # some action code because we're in a custom resource
+      end
+    RUBY
+  end
+
+  it 'does not register an offense with a custom resource that does not use allowed_actions' do
+    expect_no_violations(<<-RUBY)
+      property :something, String
+
+      action :create do
+        # some action code because we're in a custom resource
+      end
+    RUBY
+  end
+
+  it 'does not register an offense with a LWRP that uses allowed_actions' do
+    expect_no_violations(<<-RUBY)
+      attribute :something, String
+
+      allowed_actions [:create, :remove]
+    RUBY
+  end
+end
