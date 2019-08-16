cb4647dc7fd9a126003866e5173abb0c22b97c84
Tim Smith <tsmith84@gmail.com>
2019-08-16 15:52:37 -0700

    Allow for resource_name to be nil in the helper
    This way you can look for a specific property in any resource
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/rubocop/chef/cookbook_helpers.rb
+++ lib/rubocop/chef/cookbook_helpers.rb
@@ -11,18 +11,16 @@ module RuboCop
       # @yield
       #
       def match_property_in_resource?(resource_name, property_name, node)
-        # make sure what we were passed is a symbol
-        resource_name = resource_name.to_sym
-        property_name = property_name.to_sym
-
-        # bail out if we're not in the resource we care about
         return unless node.block_type? # resources are blocks if they have properties
-        return unless node.children.first.method?(resource_name) # see if we're in the right resource
+        return unless node.children.first.receiver.nil? # resource blocks don't have a receiver
+
+        # bail out if we're not in the resource we care about or nil was passed (all resources)
+        return unless resource_name.nil? || node.children.first.method?(resource_name.to_sym) # see if we're in the right resource
 
         if node.children[2] && node.children[2].begin_type? # nil would be an empty block. begin_type is the parent of our properties
           node.children[2].children.each do |property|
             extract_properties(property) do |p|
-              yield(p) if p.method_name == property_name
+              yield(p) if p.method_name == property_name.to_sym
             end
           end
         end
