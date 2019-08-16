0bbc2a9ef019c4659060a20838de2c215a9bb7d4
Tim Smith <tsmith84@gmail.com>
2019-08-16 15:53:07 -0700

    Add Chef/ResourceWithNoneAction
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/correctness/resource_with_none_action.rb
@@ -0,0 +1,50 @@
+#
+# Copyright:: 2019, Chef Software Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module RuboCop
+  module Cop
+    module Chef
+      # The :nothing action is often typo'd as :nothing
+      #
+      # @example
+      #
+      #   # bad
+      #   service 'foo' do
+      #    action :none
+      #   end
+      #
+      #   # good
+      #   service 'foo' do
+      #    action :nothing
+      #   end
+      #
+      class ResourceWithNoneAction < Cop
+        include RuboCop::Chef::CookbookHelpers
+
+        MSG = 'Resource uses the :none action instead of :nothing'.freeze
+
+        def on_block(node)
+          match_property_in_resource?(nil, 'action', node) do |action_node|
+            action_node.arguments.each do |action|
+              add_offense(action, location: :expression, message: MSG, severity: :refactor) if action.source == ":none"
+            end
+          end
+        end
+      end
+    end
+  end
+end
