b3d15c1e531438644119d2a9b420462f8af72b47
Tim Smith <tsmith84@gmail.com>
2019-08-17 14:18:35 -0700

    Add 3 new cops for detecting legacy recipes being included
    Chef/IncludingAptDefaultRecipe
    Chef/IncludingWindowsDefaultRecipe
    Chef/IncludingYumDNFCompatRecipe
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/modernize/apt_default_recipe.rb
@@ -0,0 +1,47 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module RuboCop
+  module Cop
+    module Chef
+      # Don't include the apt default recipe to update apt's package cache when you can
+      # use the apt_update resource built into Chef Infra Client 12.7 and later.
+      #
+      # @example
+      #
+      #   # bad
+      #   include_recipe 'apt::default'
+      #   include_recipe 'apt'
+      #
+      #   # good
+      #   apt_update
+      #
+      class IncludingAptDefaultRecipe < Cop
+        MSG = 'Do not include the Apt default recipe to update package cache. Instead use the apt_update resource, which is built into Chef Infra Client 12.7 and later.'.freeze
+
+        def_node_matcher :apt_recipe_usage?, <<-PATTERN
+          (send nil? :include_recipe (str {"apt" "apt::default"}))
+        PATTERN
+
+        def on_send(node)
+          apt_recipe_usage?(node) do
+            add_offense(node, location: :expression, message: MSG, severity: :refactor)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/rubocop/cop/chef/modernize/windows_default_recipe.rb
@@ -0,0 +1,50 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module RuboCop
+  module Cop
+    module Chef
+      # Don't include the windows default recipe that is either full of gem install that are part
+      # of the Chef Infra Client, or empty (depends on version).
+      #
+      # @example
+      #
+      #   # bad
+      #   include_recipe 'windows::default'
+      #   include_recipe 'windows'
+      #
+      class IncludingWindowsDefaultRecipe < Cop
+        MSG = 'Do not include the Windows default recipe, which only installs win32 gems already included in Chef Infra Client'.freeze
+
+        def_node_matcher :windows_recipe_usage?, <<-PATTERN
+          (send nil? :include_recipe (str {"windows" "windows::default"}))
+        PATTERN
+
+        def on_send(node)
+          windows_recipe_usage?(node) do
+            add_offense(node, location: :expression, message: MSG, severity: :refactor)
+          end
+        end
+
+        def autocorrect(node)
+          lambda do |corrector|
+            corrector.remove(node.loc.expression)
+          end
+        end
+      end
+    end
+  end
+end
