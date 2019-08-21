e4ba801dfab3fbada280d1a4c9ad4630405e9be9
Tim Smith <tsmith84@gmail.com>
2019-08-20 22:37:17 -0400

    Add Chef/NodeMethodsInsteadofAttributes cop
    This detects changes due our the removal of the node method missing behavior in Chef 13. I'm not calling it that because no one knows what method missing is who just needs to clean up some cookbooks. This catches the bad methods we can be 100% sure of. There's 250 of these on the supermarket. Obviously it's not going to catch all your cookbook attributes, but it gets you a bit closer.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/deprecation/node_methods_not_attributes.rb
@@ -0,0 +1,59 @@
+#
+# Copyright:: 2019, Chef Software Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module RuboCop
+  module Cop
+    module Chef
+      # Incorrectly using node methods for Ohai data when you really want node attributes
+      #
+      # @example
+      #
+      #   # bad
+      #   node.fqdn
+      #   node.platform
+      #   node.platform_family
+      #   node.platform_version
+      #   node.hostname
+      #
+      #   # good
+      #   node['fqdn']
+      #   node['platform']
+      #   node['platform_family']
+      #   node['platform_version']
+      #   node['hostname']
+      #
+      class NodeMethodsInsteadofAttributes < Cop
+        MSG = 'Use node attributes to access Ohai data instead of node methods, which were deprecated in Chef Infra Client 13.'.freeze
+
+        def_node_matcher :node_ohai_methods?, <<-PATTERN
+          (send (send nil? :node) ${:fqdn :platform :platform_version :platform_family :hostname})
+        PATTERN
+
+        def on_send(node)
+          node_ohai_methods?(node) do
+            add_offense(node, location: :selector, message: MSG, severity: :refactor)
+          end
+        end
+
+        def autocorrect(node)
+          lambda do |corrector|
+            corrector.replace(node.loc.expression, "node['#{node.method_name}']")
+          end
+        end
+      end
+    end
+  end
+end
