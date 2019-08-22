8e4df45aca6be75cff8af60020303bcca2e8a1a8
Tim Smith <tsmith84@gmail.com>
2019-08-22 14:38:22 -0400

    Add Chef/MinitestHandlerUsage Chef/WindowsVersionHelper Chef/WindowsZipfileUsage
    Add three more cops for
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/modernize/minitest_handler_usage.rb
@@ -0,0 +1,48 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module RuboCop
+  module Cop
+    module Chef
+      # Use Chef InSpec for testing instead of the Minitest Handler cookbook pattern.
+      #
+      # @example
+      #
+      #   # bad
+      #   depends 'minitest-handler'
+      #
+      class MinitestHandlerUsage < Cop
+        MSG = 'Use Chef InSpec for testing instead of the Minitest Handler cookbook pattern.'.freeze
+
+        def_node_matcher :minitest_depends?, <<-PATTERN
+          (send nil? :depends (str "minitest-handler"))
+        PATTERN
+
+        def on_send(node)
+          minitest_depends?(node) do
+            add_offense(node, location: :expression, message: MSG, severity: :refactor)
+          end
+        end
+
+        def autocorrect(node)
+          lambda do |corrector|
+            corrector.remove(node.loc.expression)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/chef/modernize/minitest_handler_usage_spec.rb
@@ -0,0 +1,34 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::MinitestHandlerUsage, :config do
+  subject(:cop) { described_class.new(config) }
+
+  it 'registers an offense when a cookbook depends on "build-essential"' do
+    expect_offense(<<~RUBY)
+      depends 'minitest-handler'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^ Use Chef InSpec for testing instead of the Minitest Handler cookbook pattern.
+    RUBY
+  end
+
+  it "doesn't register an offense when depending on any old cookbook" do
+    expect_no_offenses(<<~RUBY)
+      depends 'build-essentially'
+    RUBY
+  end
+end
