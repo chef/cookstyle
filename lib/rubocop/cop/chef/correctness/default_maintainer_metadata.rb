2e2e5c8ef76d8236054b57583ed443ae612b82ce
Tim Smith <tsmith84@gmail.com>
2019-08-23 20:15:02 -0500

    Add Chef/DefaultMetadataMaintainer
    This is a port of a Foodcritic rule
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/correctness/default_maintainer_metadata.rb
@@ -0,0 +1,48 @@
+#
+# Copyright:: 2019, Chef Software Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module RuboCop
+  module Cop
+    module Chef
+      # Metadata contains default maintainer information from the `chef generate cookbook`
+      # command. This should be updated to reflect that actual maintainer of the cookbook.
+      #
+      # @example
+      #
+      #   # bad
+      #   maintainer 'YOUR_COMPANY_NAME'
+      #   maintainer_email 'YOUR_EMAIL'
+      #
+      #   # good
+      #   maintainer 'Bob Bobberson'
+      #   maintainer_email 'bob@bobberson.com'
+      #
+
+      class DefaultMetadataMaintainer < Cop
+        MSG = 'Metadata contains default maintainer information from the cookbook generator. Add actual cookbook maintainer information to the metadata.rb.'.freeze
+
+        def_node_matcher :default_metadata?, '(send nil? {:maintainer :maintainer_email} (str {"YOUR_COMPANY_NAME" "YOUR_EMAIL"}))'
+
+        def on_send(node)
+          default_metadata?(node) do
+            add_offense(node, location: :expression, message: MSG, severity: :refactor)
+          end
+        end
+      end
+    end
+  end
+end
