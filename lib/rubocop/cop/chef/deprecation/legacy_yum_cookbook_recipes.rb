f3b4adc0a15eb41bc5f735fb7a773e3e34a16015
Tim Smith <tsmith84@gmail.com>
2019-08-26 15:12:06 -0700

    Add Chef/IncludingOhaiDefaultRecipe Chef/IncludingXMLRubyRecipe & Chef/LegacyYumCookbookRecipes
    LegacyYumCookbookRecipes: Determines when the old yum cookbook recipes from yum < 3.0 is used.
    IncludingOhaiDefaultRecipe: Determines if a user is including the old default recipe from the ohai cookbook which required forking that cookbook
    IncludingXMLRubyRecipe: detects using the xml cookbook to compile the nokogiri gem into Chef. That's built into Chef 12+ anyways
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/deprecation/legacy_yum_cookbook_recipes.rb
@@ -0,0 +1,49 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module RuboCop
+  module Cop
+    module Chef
+      # The Ohai default recipe previously allowed a user to ship custom Ohai plugins to a system by including them
+      # in a directory in the Ohai cookbook. This functionality was replaced with the ohai_plugin resource, which
+      # should be used instead as it doesn't require forking the ohai cookbook.
+      #
+      # @example
+      #
+      #   # bad
+      #   include_recipe 'yum::elrepo'
+      #   include_recipe 'yum::epel'
+      #   include_recipe 'yum::ius'
+      #   include_recipe 'yum::remi'
+      #   include_recipe 'yum::repoforge'
+      #   include_recipe 'yum::yum'
+      #
+      class LegacyYumCookbookRecipes < Cop
+        MSG = 'The elrepo, epel, ius, remi, and repoforge recipes were split into their own cookbooks and the yum recipe was renamed to be default with the release of yum cookbook 3.0 (Dec 2013).'.freeze
+
+        def_node_matcher :old_yum_recipe?, <<-PATTERN
+          (send nil? :include_recipe (str {"yum::elrepo" "yum::epel" "yum::ius" "yum::remi" "yum::repoforge" "yum::yum"}))
+        PATTERN
+
+        def on_send(node)
+          old_yum_recipe?(node) do
+            add_offense(node, location: :expression, message: MSG, severity: :refactor)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/chef/deprecation/legacy_yum_cookbook_spec.rb
@@ -0,0 +1,69 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::LegacyYumCookbookRecipes, :config do
+  subject(:cop) { described_class.new(config) }
+
+  it 'registers an offense when a cookbook includes "yum::elrepo"' do
+    expect_offense(<<~RUBY)
+      include_recipe 'yum::elrepo'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ The elrepo, epel, ius, remi, and repoforge recipes were split into their own cookbooks and the yum recipe was renamed to be default with the release of yum cookbook 3.0 (Dec 2013).
+    RUBY
+  end
+
+  it 'registers an offense when a cookbook includes "yum::epel"' do
+    expect_offense(<<~RUBY)
+      include_recipe 'yum::epel'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^ The elrepo, epel, ius, remi, and repoforge recipes were split into their own cookbooks and the yum recipe was renamed to be default with the release of yum cookbook 3.0 (Dec 2013).
+    RUBY
+  end
+
+  it 'registers an offense when a cookbook includes "yum::ius"' do
+    expect_offense(<<~RUBY)
+      include_recipe 'yum::ius'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^ The elrepo, epel, ius, remi, and repoforge recipes were split into their own cookbooks and the yum recipe was renamed to be default with the release of yum cookbook 3.0 (Dec 2013).
+    RUBY
+  end
+
+  it 'registers an offense when a cookbook includes "yum::remi"' do
+    expect_offense(<<~RUBY)
+      include_recipe 'yum::remi'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^ The elrepo, epel, ius, remi, and repoforge recipes were split into their own cookbooks and the yum recipe was renamed to be default with the release of yum cookbook 3.0 (Dec 2013).
+    RUBY
+  end
+
+  it 'registers an offense when a cookbook includes "yum::repoforge"' do
+    expect_offense(<<~RUBY)
+      include_recipe 'yum::repoforge'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ The elrepo, epel, ius, remi, and repoforge recipes were split into their own cookbooks and the yum recipe was renamed to be default with the release of yum cookbook 3.0 (Dec 2013).
+    RUBY
+  end
+
+  it 'registers an offense when a cookbook includes "yum::yum"' do
+    expect_offense(<<~RUBY)
+      include_recipe 'yum::yum'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^ The elrepo, epel, ius, remi, and repoforge recipes were split into their own cookbooks and the yum recipe was renamed to be default with the release of yum cookbook 3.0 (Dec 2013).
+    RUBY
+  end
+
+  it "doesn't register an offense when including yum::default" do
+    expect_no_offenses(<<~RUBY)
+      include_recipe 'yum::default'
+    RUBY
+  end
+end
