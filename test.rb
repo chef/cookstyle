0edca1945890883f887df6e19c3e722de7869454
Tim Smith <tsmith84@gmail.com>
2019-08-29 10:55:41 -0700

    Remove my test file I added by accident
    Now you know how I test initially
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test.rb
+++
@@ -1,9 +0,0 @@
-# node.save
-
-use_inline_resources
-
-# use_inline_resources if defined?(use_inline_resources)
-
-use_inline_resources if defined?(use_inline_resources2)
-
-# (if (defined? (send nil :use_inline_resources)) (send nil :use_inline_resources) nil)
