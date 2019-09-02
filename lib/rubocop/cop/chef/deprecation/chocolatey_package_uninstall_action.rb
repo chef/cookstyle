010ed5ae507853032925446b5e9d3d8d9667715c
Tim Smith <tsmith84@gmail.com>
2019-09-02 13:10:54 -0700

    Add Chef/ChocolateyPackageUninstallAction and Chef/LaunchdDeprecatedHashProperty
    Detect a few more deprecations
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/deprecation/chocolatey_package_uninstall_action.rb
@@ -0,0 +1,50 @@
+#
+# Copyright:: 2019, Chef Software Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module RuboCop
+  module Cop
+    module Chef
+      # Use the :remove action in the chocolatey_package resource instead of :uninstall which was removed in Chef Infra Client 14+
+      #
+      # @example
+      #
+      #   # bad
+      #   chocolatey_package 'nginx' do
+      #     action :uninstall
+      #   end
+      #
+      #   # good
+      #   chocolatey_package 'nginx' do
+      #     action :remove
+      #   end
+      #
+      class ChocolateyPackageUninstallAction < Cop
+        include RuboCop::Chef::CookbookHelpers
+
+        MSG = 'Use the :remove action in the chocolatey_package resource instead of :uninstall which was removed in Chef Infra Client 14+'.freeze
+
+        def on_block(node)
+          match_property_in_resource?(:chocolatey_package, 'action', node) do |choco_action|
+            choco_action.arguments.each do |action|
+              add_offense(action, location: :expression, message: MSG, severity: :refactor) if action.source == ':uninstall'
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/rubocop/cop/chef/deprecation/launchd_deprecated_hash_property.rb
@@ -0,0 +1,47 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module RuboCop
+  module Cop
+    module Chef
+      # The launchd resource's hash property was renamed to plist_hash in Chef Infra Client 13+ to avoid conflicts with Ruby's hash class.
+      #
+      # @example
+      #
+      #   # bad
+      #   launchd 'foo' do
+      #     hash foo: 'bar'
+      #   end
+      #
+      #   # good
+      #   launchd 'foo' do
+      #     plist_hash foo: 'bar'
+      #   end
+      #
+      class LaunchdDeprecatedHashProperty < Cop
+        include RuboCop::Chef::CookbookHelpers
+
+        MSG = "The launchd resource's hash property was renamed to plist_hash in Chef Infra Client 13+ to avoid conflicts with Ruby's hash class.".freeze
+
+        def on_block(node)
+          match_property_in_resource?(:launchd, 'hash', node) do |hash_prop|
+            add_offense(hash_prop, location: :expression, message: MSG, severity: :refactor)
+          end
+        end
+      end
+    end
+  end
+end
