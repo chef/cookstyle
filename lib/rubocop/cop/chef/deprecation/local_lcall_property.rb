2ecec6c2d8ea0e4d7cafbaeb35778d7357ba1845
Tim Smith <tsmith84@gmail.com>
2019-09-02 13:21:37 -0700

    Add Chef/LocaleDeprecatedLcallProperty Cop
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/deprecation/local_lcall_property.rb
@@ -0,0 +1,42 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module RuboCop
+  module Cop
+    module Chef
+      # The supports property was removed in Chef Infra Client 13 in favor of individual 'manage_home' and 'non_unique' properties.
+      #
+      # @example
+      #
+      #   # bad
+      #   locale 'foo' do
+      #     lc_all 'bar'
+      #   end
+      #
+      class LocaleDeprecatedLcallProperty < Cop
+        include RuboCop::Chef::CookbookHelpers
+
+        MSG = "The local resource's lc_all property has been deprecated and will be removed in Chef Infra Client 16".freeze
+
+        def on_block(node)
+          match_property_in_resource?(:locale, 'lc_all', node) do |property|
+            add_offense(property, location: :expression, message: MSG, severity: :refactor)
+          end
+        end
+      end
+    end
+  end
+end
