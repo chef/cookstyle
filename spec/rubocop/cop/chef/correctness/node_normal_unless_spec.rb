e6f2e1b84a2972c2637746977a63d3feccca442f
Tim Smith <tsmith84@gmail.com>
2019-09-02 15:04:14 -0700

    Add specs for NodeNormal and NodeNormalUnless
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ spec/rubocop/cop/chef/correctness/node_normal_unless_spec.rb
@@ -0,0 +1,55 @@
+#
+# Copyright:: Copyright 2019, Chef Software Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::NodeNormalUnless do
+  subject(:cop) { described_class.new }
+
+  it 'registers an offense on using node.normal_unless' do
+    expect_offense(<<~RUBY)
+      node.normal_unless[:foo]
+      ^^^^^^^^^^^^^^^^^^ Do not use node.normal_unless. Replace with default/override/force_default/force_override attribute levels.
+    RUBY
+
+    expect_correction(<<~RUBY)
+    node.normal_unless[:foo]
+    RUBY
+  end
+
+  it 'registers an offense on using chef_run.node.normal_unless' do
+    expect_offense(<<~RUBY)
+      chef_run.node.normal_unless[:foo]
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Do not use node.normal_unless. Replace with default/override/force_default/force_override attribute levels.
+    RUBY
+
+    expect_correction(<<~RUBY)
+    chef_run.node.normal_unless[:foo]
+    RUBY
+  end
+
+  it 'registers no offense on using chef_run.normal_unless' do
+    expect_no_offenses(<<~RUBY)
+      chef_run.normal_unless[:foo]
+    RUBY
+  end
+
+  it 'registers no offense on using node.default_unless' do
+    expect_no_offenses(<<~RUBY)
+      node.default_unless[:foo]
+    RUBY
+  end
+end
