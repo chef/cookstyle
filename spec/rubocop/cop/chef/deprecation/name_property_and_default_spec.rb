51697b81f0ebfe6d3bd33035b4ad6e80613cff23
Tim Smith <tsmith84@gmail.com>
2019-10-03 21:53:33 -0700

    Add specs for new cops
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ spec/rubocop/cop/chef/deprecation/name_property_and_default_spec.rb
@@ -0,0 +1,40 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::ChefDeprecations::NamePropertyWithDefaultValue, :config do
+  subject(:cop) { described_class.new(config) }
+
+  it 'registers an offense when a resource property is both a name_property and has a default' do
+    expect_offense(<<~RUBY)
+      property :foo, String, name_property: true, default: 'foo'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ A resource property can't be marked as a name_property and also have a default value. This will fail in Chef Infra Client 13 or later.
+    RUBY
+  end
+
+  it "doesn't register an offense with non-name_property property with a default" do
+    expect_no_offenses(<<~RUBY)
+      property :foo, String, default: 'foo'
+    RUBY
+  end
+
+  it "doesn't register an offense with name_property that does not have a default" do
+    expect_no_offenses(<<~RUBY)
+      property :foo, String, name_property: true
+    RUBY
+  end
+end
---
+++ spec/rubocop/cop/chef/deprecation/resource_overrides_provides_method_spec.rb
@@ -0,0 +1,40 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::ChefDeprecations::ResourceOverridesProvidesMethod, :config do
+  subject(:cop) { described_class.new(config) }
+
+  it 'registers an offense when a resource overrides provides? w/o using provides' do
+    expect_offense(<<~RUBY)
+      def provides?
+      ^^^^^^^^^^^^^ Don't override the provides? method in a resource provider without also calling provides :SOME_PROVIDER_NAME. This will cause failures in Chef Infra Client 13 and later.
+        true
+      end
+    RUBY
+  end
+
+  it "doesn't register an offense when overriding provides? and using provides" do
+    expect_no_offenses(<<~RUBY)
+      provides :my_cool_provider
+
+      def provides?
+       true
+      end
+    RUBY
+  end
+end
