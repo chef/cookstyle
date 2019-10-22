28cddb61722559d9b8e05ee2489bcf5b5424dcc4
Tim Smith <tsmith84@gmail.com>
2019-10-22 11:32:34 -0700

    Add 3 new cops for migrating to Effortless
    A few more disabled by default cops to help folks to upgrade.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/effortless/node_environment.rb
@@ -0,0 +1,40 @@
+#
+# Copyright:: Copyright 2019, Chef Software Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module RuboCop
+  module Cop
+    module Chef
+      module ChefEffortless
+        # Neither Policyfiles or Effortless Infra which is based on Policyfiles supports Chef Environments
+        #
+        # @example
+        #
+        #   # bad
+        #   node.environment == "production"
+        #
+        class CookbookUsesEnvironmments < Cop
+          MSG = 'Cookbook uses environments, which cannot be used in Policyfiles or Effortless Infra'.freeze
+
+          def on_send(node)
+            if node.method_name == :environment && node.receiver && node.receiver.send_type? && node.receiver.method_name == :node
+              add_offense(node, location: :expression, message: MSG, severity: :refactor)
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/chef/effortless/node_policygroup_spec.rb
@@ -0,0 +1,29 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::ChefEffortless::CookbookUsesPolicygroups, :config do
+  subject(:cop) { described_class.new(config) }
+
+  it 'registers an offense when node.policygroup is used' do
+    expect_offense(<<~RUBY)
+      node.policy_group == 'webserver'
+      ^^^^^^^^^^^^^^^^^ Cookbook uses Policy Groups, which cannot be used with Effortless Infra
+    RUBY
+  end
+end
