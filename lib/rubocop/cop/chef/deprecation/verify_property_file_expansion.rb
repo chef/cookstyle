d1c2eaecb7396951baa199a3611acf315b07be19
Tim Smith <tsmith84@gmail.com>
2019-10-22 15:08:34 -0700

    Add ChefDeprecations/VerifyPropertyUsesFileExpansion cop
    Detects incorrect verify property usage that breaks Chef 13+
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/deprecation/verify_property_file_expansion.rb
@@ -0,0 +1,55 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module RuboCop
+  module Cop
+    module Chef
+      module ChefDeprecations
+        # In Chef Infra Client 13 the "file" variable for use within the verify property was replaced with the "path" variable.
+        #
+        # @example
+        #
+        #   # bad
+        #   file '/etc/nginx.conf' do
+        #     verify 'nginx -t -c %{file}'
+        #   end
+        #
+        #   # good
+        #   file '/etc/nginx.conf' do
+        #     verify 'nginx -t -c %{path}'
+        #   end
+        #
+        class VerifyPropertyUsesFileExpansion < Cop
+          include RuboCop::Chef::CookbookHelpers
+
+          MSG = "Use the 'path' variable in the verify property and not the 'file' property which was removed in Chef Infra Client 13.".freeze
+
+          def on_block(node)
+            match_property_in_resource?(nil, 'verify', node) do |verify|
+              add_offense(verify, location: :expression, message: MSG, severity: :refactor) if verify.source.match?(/%{file}/)
+            end
+          end
+
+          def autocorrect(node)
+            lambda do |corrector|
+              corrector.replace(node.loc.expression, node.loc.expression.source.gsub('%{file}', '%{path}'))
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/chef/deprecation/verify_property_file_expansion_spec.rb
@@ -0,0 +1,45 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::ChefDeprecations::VerifyPropertyUsesFileExpansion, :config do
+  subject(:cop) { described_class.new(config) }
+
+  it 'registers an offense when using the file variable in the verify property' do
+    expect_offense(<<~RUBY)
+      file '/etc/nginx.conf' do
+        verify 'nginx -t -c %{file}'
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use the 'path' variable in the verify property and not the 'file' property which was removed in Chef Infra Client 13.
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      file '/etc/nginx.conf' do
+        verify 'nginx -t -c %{path}'
+      end
+    RUBY
+  end
+
+  it "doesn't register an when usinng path variable in the verify property" do
+    expect_no_offenses(<<~RUBY)
+      file '/etc/nginx.conf' do
+        verify 'nginx -t -c %{path}'
+      end
+    RUBY
+  end
+end
