bb5e9a8bd3e4352359aa2939ce11ac9110ffbbdf
Tim Smith <tsmith84@gmail.com>
2019-10-23 12:58:34 -0700

    Add ChefDeprecations/LegacyYumRepositoryProperties
    Detect yum cookbook pre-3.0 properties.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/deprecation/deprecated_yum_repository_properties.rb
@@ -0,0 +1,65 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module RuboCop
+  module Cop
+    module Chef
+      module ChefDeprecations
+        # With the release of Chef Infra Client 12.14 and the yum cookbook 3.0 several properties in the yum_repository resource were renamed. url -> baseurl, keyurl -> gpgkey, and mirrorexpire -> mirror_expire.
+        #
+        # @example
+        #
+        #   # bad
+        #   yum_repository 'OurCo' do
+        #     description 'OurCo yum repository'
+        #     url 'http://artifacts.ourco.org/foo/bar'
+        #     keyurl 'http://artifacts.ourco.org/pub/yum/RPM-GPG-KEY-OURCO-6'
+        #     mirrorexpire 1440
+        #     action :create
+        #   end
+        #
+        #   # good
+        #   yum_repository 'OurCo' do
+        #     description 'OurCo yum repository'
+        #     baseurl 'http://artifacts.ourco.org/foo/bar'
+        #     gpgkey 'http://artifacts.ourco.org/pub/yum/RPM-GPG-KEY-OURCO-6'
+        #     mirror_expire 1440
+        #     action :create
+        #   end
+        #
+        class DeprecatedYumRepositoryProperties < Cop
+          include RuboCop::Chef::CookbookHelpers
+
+          MSG = "With the release of Chef Infra Client 12.14 and the yum cookbook 3.0 several properties in the yum_repository resource were renamed. url -> baseurl, keyurl -> gpgkey, and mirrorexpire -> mirror_expire.".freeze
+
+          def on_block(node)
+            %w(url keyurl mirrorexpire).each do |prop|
+              match_property_in_resource?(:yum_repository, prop, node) do |prop_node|
+                add_offense(prop_node, location: :expression, message: MSG, severity: :refactor)
+              end
+            end
+          end
+
+          def autocorrect(node)
+            lambda do |corrector|
+              corrector.replace(node.loc.expression, node.loc.expression.source.gsub(/^url/, 'baseurl').gsub(/^keyurl/, 'gpgkey').gsub(/^mirrorexpire/, 'mirror_expire'))
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/chef/deprecation/deprecated_yum_repository_properties_spec.rb
@@ -0,0 +1,59 @@
+#
+# Copyright:: 2019, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::ChefDeprecations::DeprecatedYumRepositoryProperties, :config do
+  subject(:cop) { described_class.new(config) }
+
+  it 'registers an offense when the using legacy yum_repository properties' do
+    expect_offense(<<~RUBY)
+      yum_repository 'OurCo' do
+        description 'OurCo yum repository'
+        url 'http://artifacts.ourco.org/foo/bar'
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ With the release of Chef Infra Client 12.14 and the yum cookbook 3.0 several properties in the yum_repository resource were renamed. url -> baseurl, keyurl -> gpgkey, and mirrorexpire -> mirror_expire.
+        keyurl 'http://artifacts.ourco.org/pub/yum/RPM-GPG-KEY-OURCO-6'
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ With the release of Chef Infra Client 12.14 and the yum cookbook 3.0 several properties in the yum_repository resource were renamed. url -> baseurl, keyurl -> gpgkey, and mirrorexpire -> mirror_expire.
+        mirrorexpire 1440
+        ^^^^^^^^^^^^^^^^^ With the release of Chef Infra Client 12.14 and the yum cookbook 3.0 several properties in the yum_repository resource were renamed. url -> baseurl, keyurl -> gpgkey, and mirrorexpire -> mirror_expire.
+        action :create
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      yum_repository 'OurCo' do
+        description 'OurCo yum repository'
+        baseurl 'http://artifacts.ourco.org/foo/bar'
+        gpgkey 'http://artifacts.ourco.org/pub/yum/RPM-GPG-KEY-OURCO-6'
+        mirror_expire 1440
+        action :create
+      end
+    RUBY
+  end
+
+  it "doesn't register an offense when using new yum_repository properties" do
+    expect_no_offenses(<<~RUBY)
+      yum_repository 'OurCo' do
+        description 'OurCo yum repository'
+        baseurl 'http://artifacts.ourco.org/foo/bar'
+        gpgkey 'http://artifacts.ourco.org/pub/yum/RPM-GPG-KEY-OURCO-6'
+        mirror_expire 1440
+        action :create
+      end
+    RUBY
+  end
+end
