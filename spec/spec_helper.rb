83b51d1e56b2589982305c7402b4b67b1d2f6cba
Tim Smith <tsmith84@gmail.com>
2019-12-05 11:52:01 -0800

    Remove some unnecessary rspec helpers
    These came from rubocop-rspec and aren't needed here
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,14 +14,6 @@ RSpec.configure do |config|
   config.filter_run(:focus)
   config.order = :random
 
-  config.expect_with :rspec do |expectations|
-    expectations.syntax = :expect # Disable `should`
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.syntax = :expect # Disable `should_receive` and `stub`
-  end
-
   config.include RuboCop::RSpec::ExpectOffense
 end
 
