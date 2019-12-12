a3847d6d286b5a67ca3f5f04b71c37688bef8d25
Tim Smith <tsmith84@gmail.com>
2019-12-12 15:20:08 -0800

    Rework things so we don't introduce weird extra spaces
    This allows us to keep all our existing formatting without the need to run other cops
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/rubocop/cop/chef/style/unnecessary_platform_case_statement.rb
+++ lib/rubocop/cop/chef/style/unnecessary_platform_case_statement.rb
@@ -36,6 +36,8 @@ module RuboCop
         #   end
         #
         class UnnecessaryPlatformCaseStatement < Cop
+          include RangeHelp
+
           MSG = 'Use the platform?() and platform_family?() helpers instead of a case statement that only includes a single when statement.'.freeze
 
           def_node_matcher :platform_case?, <<-PATTERN
@@ -53,18 +55,23 @@ module RuboCop
               platform_case?(node) do |node_, type|
                 condition_string = node.when_branches.first.conditions.map(&:source).join(', ')
 
-                # single line bodies should be transformed into `X if platform?('ubuntu')` style statements
+                # single line bodies without an else statement should be transformed into `X if platform?('ubuntu')` style statements
                 # while multiline statements should just have the case and when bits replace with `if platform?('ubuntu')`
-                if node.when_branches.first.body.multiline?
-                  corrector.remove(node.loc.keyword) # case
-                  corrector.remove(node_.loc.expression) # node['whatever']
-                  corrector.remove(node.when_branches.first.loc.keyword) # when
-
-                  conditional_range = node.when_branches.first.conditions[0].source_range.join(node.when_branches.first.conditions[-1].source_range.end)
-                  corrector.replace(conditional_range, "if #{type.value}?(#{condition_string})")
-                else
+                if !node.else? && !node.when_branches.first.body.multiline?
                   new_source = "#{node.when_branches.first.body.source} if #{type.value}?(#{condition_string})"
                   corrector.replace(node.loc.expression, new_source)
+                else
+                  # find the range from the beginning of the case to the end of the node['platform'] or node['platform_family']
+                  case_range = node.loc.keyword.join(node_.loc.expression.end)
+
+                  # replace the complete conditional range with a new if statement
+                  corrector.replace(case_range, "if #{type.value}?(#{condition_string})")
+
+                  # find the range from the start of the when to the end of the last argument
+                  conditional_range = node.when_branches.first.conditions[-1].source_range.join(node.when_branches.first.loc.keyword.begin)
+
+                  # remove the when XYZ condition along with any leading spaces so that we remove the whole empty line
+                  corrector.remove(range_with_surrounding_space(range: conditional_range, side: :left))
                 end
               end
             end
