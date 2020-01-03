ec8d8527017255a7dccd03998a39c386aeaf7ddb
Tim Smith <tsmith84@gmail.com>
2020-01-03 15:30:11 -0800

    Better trim metadata.rb whitespace
    Neither right or left gets it all and :both mangles the world. Left seems to result in better output when the metadata is nested between other metadata which is the most common scenario. I updated the specs to reflect a real metadata.rb file.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/rubocop/cop/chef/redundant/conflicts_metadata_spec.rb
+++ spec/rubocop/cop/chef/redundant/conflicts_metadata_spec.rb
@@ -21,11 +21,16 @@ describe RuboCop::Cop::Chef::ChefRedundantCode::ConflictsMetadata, :config do
 
   it 'registers an offense when metadata uses "conflicts"' do
     expect_offense(<<~RUBY)
+      name 'foo'
       conflicts 'foo'
       ^^^^^^^^^^^^^^^ The conflicts metadata.rb method is not used and is unnecessary in cookbooks.
+      depends 'bar'
     RUBY
 
-    expect_correction('')
+    expect_correction(<<~RUBY)
+      name 'foo'
+      depends 'bar'
+    RUBY
   end
 
   it "doesn't register an offense on normal metadata" do
