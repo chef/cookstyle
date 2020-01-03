b386be280c2d5d60151781d5d7b290ef91ae1cec
Tim Smith <tsmith84@gmail.com>
2020-01-03 15:28:07 -0800

    Add ChefRedundantCode/GroupingMetadata
    I missed this obscure unused metadata
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ spec/rubocop/cop/chef/redundant/grouping_metadata_spec.rb
@@ -0,0 +1,42 @@
+#
+# Copyright:: 2020, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::ChefRedundantCode::GroupingMetadata, :config do
+  subject(:cop) { described_class.new(config) }
+
+  it 'registers an offense when metadata uses "grouping"' do
+    expect_offense(<<~RUBY)
+      name 'foo'
+      grouping 'foo'
+      ^^^^^^^^^^^^^^ The grouping metadata.rb method is not used and is unnecessary in cookbooks.
+      depends 'bar'
+    RUBY
+
+    expect_correction(<<~RUBY)
+      name 'foo'
+      depends 'bar'
+    RUBY
+  end
+
+  it "doesn't register an offense on normal metadata" do
+    expect_no_offenses(<<~RUBY)
+      depends 'foo'
+    RUBY
+  end
+end
