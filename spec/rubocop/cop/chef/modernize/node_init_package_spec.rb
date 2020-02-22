53773b36fd1bc1572c5e948f7f823af946f84025
Tim Smith <tsmith84@gmail.com>
2020-02-22 14:57:37 -0800

    Use node['init_package'] to detect systemd
    This reduces some crazy complex code that hasn't been necessary since Chef 12.0.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ spec/rubocop/cop/chef/modernize/node_init_package_spec.rb
@@ -0,0 +1,116 @@
+#
+# Copyright:: 2020, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::ChefModernize::NodeInitPackage, :config do
+  subject(:cop) { described_class.new(config) }
+
+  it "registers an offense with ::File.open('/proc/1/comm').gets.chomp" do
+    expect_offense(<<~RUBY)
+      ::File.open('/proc/1/comm').gets.chomp == 'systemd'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use node['init_package'] to check for systemd instead of reading the contents of '/proc/1/comm'
+    RUBY
+
+    expect_correction(<<~RUBY)
+      node['init_package'] == 'systemd'
+    RUBY
+  end
+
+  it "registers an offense with File.open('/proc/1/comm').gets.chomp" do
+    expect_offense(<<~RUBY)
+      File.open('/proc/1/comm').gets.chomp == 'systemd'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use node['init_package'] to check for systemd instead of reading the contents of '/proc/1/comm'
+    RUBY
+
+    expect_correction(<<~RUBY)
+      node['init_package'] == 'systemd'
+    RUBY
+  end
+
+  it "registers an offense with File.exist?('/proc/1/comm') && File.open('/proc/1/comm').chomp == 'systemd'" do
+    expect_offense(<<~RUBY)
+      File.exist?('/proc/1/comm') && File.open('/proc/1/comm').chomp == 'systemd'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use node['init_package'] to check for systemd instead of reading the contents of '/proc/1/comm'
+    RUBY
+
+    expect_correction(<<~RUBY)
+      node['init_package'] == 'systemd'
+    RUBY
+  end
+
+  it "registers an offense with IO.read('/proc/1/comm').chomp == 'systemd'" do
+    expect_offense(<<~RUBY)
+      IO.read('/proc/1/comm').chomp == 'systemd'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use node['init_package'] to check for systemd instead of reading the contents of '/proc/1/comm'
+    RUBY
+
+    expect_correction(<<~RUBY)
+      node['init_package'] == 'systemd'
+    RUBY
+  end
+
+  it "registers an offense with ::IO.read('/proc/1/comm').chomp == 'systemd'" do
+    expect_offense(<<~RUBY)
+      ::IO.read('/proc/1/comm').chomp == 'systemd'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use node['init_package'] to check for systemd instead of reading the contents of '/proc/1/comm'
+    RUBY
+
+    expect_correction(<<~RUBY)
+      node['init_package'] == 'systemd'
+    RUBY
+  end
+
+  it "registers an offense with ::IO.read('/proc/1/comm').gets.chomp == 'systemd'" do
+    expect_offense(<<~RUBY)
+      ::IO.read('/proc/1/comm').gets.chomp == 'systemd'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use node['init_package'] to check for systemd instead of reading the contents of '/proc/1/comm'
+    RUBY
+
+    expect_correction(<<~RUBY)
+      node['init_package'] == 'systemd'
+    RUBY
+  end
+
+  it "registers an offense with File.exist?('/proc/1/comm') && File.open('/proc/1/comm').chomp == 'systemd'" do
+    expect_offense(<<~RUBY)
+      File.exist?('/proc/1/comm') && File.open('/proc/1/comm').chomp == 'systemd'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use node['init_package'] to check for systemd instead of reading the contents of '/proc/1/comm'
+    RUBY
+
+    expect_correction(<<~RUBY)
+      node['init_package'] == 'systemd'
+    RUBY
+  end
+
+  it "registers an offense with File.open('/proc/1/comm').chomp" do
+    expect_offense(<<~RUBY)
+      File.open('/proc/1/comm').chomp == 'systemd'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use node['init_package'] to check for systemd instead of reading the contents of '/proc/1/comm'
+    RUBY
+
+    expect_correction(<<~RUBY)
+      node['init_package'] == 'systemd'
+    RUBY
+  end
+
+  it "does not register an offense when comparing a non-systemd value in '/proc/1/comm'" do
+    expect_no_offenses(<<~RUBY)
+      ::File.open('/proc/1/comm').gets.chomp == 'foo'
+    RUBY
+  end
+end
