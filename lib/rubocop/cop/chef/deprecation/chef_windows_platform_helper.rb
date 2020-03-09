d4f391a460ef743034d10bd238c56e6e9f89c82e
Tim Smith <tsmith84@gmail.com>
2020-03-09 14:53:22 -0700

    Add ChefDeprecations/ChefWindowsPlatformHelper cop
    This gets us off this old windows helper and onto platform? which we can
    update to be just windows? sometime later down the line.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/deprecation/chef_windows_platform_helper.rb
@@ -0,0 +1,56 @@
+#
+# Copyright:: 2020, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module RuboCop
+  module Cop
+    module Chef
+      module ChefDeprecations
+        # Use `platform?('windows')` instead of the legacy `Chef::Platform.windows?` helper
+        #
+        # @example
+        #
+        #   # bad
+        #   Chef::Platform.windows?
+        #
+        #   # good
+        #   platform?('windows')
+        #   platform_family?('windows')
+        #
+        class ChefWindowsPlatformHelper < Cop
+          MSG = "Use `platform?('windows')` instead of the legacy `Chef::Platform.windows?` helper.".freeze
+
+          def_node_matcher :chef_platform_windows?, <<-PATTERN
+            (send
+              (const
+                (const nil? :Chef) :Platform) :windows?)
+          PATTERN
+
+          def on_send(node)
+            chef_platform_windows?(node) do
+              add_offense(node, location: :expression, message: MSG, severity: :refactor)
+            end
+          end
+
+          def autocorrect(node)
+            lambda do |corrector|
+              corrector.replace(node.loc.expression, "platform?('windows')")
+            end
+          end
+        end
+      end
+    end
+  end
+end
