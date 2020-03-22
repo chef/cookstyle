9fdf516e8611dad900c058d2c124ef2a9217031c
Tim Smith <tsmith84@gmail.com>
2020-03-21 22:30:11 -0700

    Add ChefCorrectness/ConditionalRubyShellout cop
    Turns out people are doing WAY too much work to just shellout.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/correctness/conditional_ruby_shellout.rb
@@ -0,0 +1,72 @@
+#
+# Copyright:: 2020, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module RuboCop
+  module Cop
+    module Chef
+      module ChefCorrectness
+        # Don't use Ruby to shellout in a only_if / not_if conditional when you can just shellout directly. Any string value used with only_if / not_if is executed in your system's shell and the return code of the command is the result for the not_if / only_if determination.
+        #
+        # @example
+        #
+        #   # bad
+        #   cookbook_file '/logs/foo/error.log' do
+        #     source 'error.log'
+        #     only_if { system('wget https://www.bar.com/foobar.txt -O /dev/null') }
+        #   end
+        #
+        #   cookbook_file '/logs/foo/error.log' do
+        #     source 'error.log'
+        #     only_if { shell_out('wget https://www.bar.com/foobar.txt -O /dev/null').exitstatus == 0 }
+        #   end
+        #
+        #   # good
+        #   cookbook_file '/logs/foo/error.log' do
+        #     source 'error.log'
+        #     only_if 'wget https://www.bar.com/foobar.txt -O /dev/null'
+        #   end
+        #
+        class ConditionalRubyShellout < Cop
+          include RuboCop::Chef::CookbookHelpers
+          MSG = "Don't use Ruby to shellout in a only_if / not_if conditional when you can just shellout directly by wrapping the command in quotes.".freeze
+
+          def_node_matcher :conditional_shellout?, <<-PATTERN
+          (block
+            (send nil? ${:only_if :not_if})
+            (args)
+            {(send nil? :system $(str ...))
+             (send (send (send nil? :shell_out $(str ...)) :exitstatus) :== (int 0))
+              })
+          PATTERN
+
+          def on_block(node)
+            conditional_shellout?(node) do
+              add_offense(node, location: :expression, message: MSG, severity: :refactor)
+            end
+          end
+
+          def autocorrect(node)
+            lambda do |corrector|
+              conditional_shellout?(node) do |type, val|
+                corrector.replace(node.loc.expression, "#{type} #{val.source}")
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/chef/correctness/conditional_ruby_shellout_spec.rb
@@ -0,0 +1,76 @@
+#
+# Copyright:: Copyright 2020, Chef Software Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::ChefCorrectness::ConditionalRubyShellout do
+  subject(:cop) { described_class.new }
+
+  it 'registers an offense with a not_if that shells out using system' do
+    expect_offense(<<~RUBY)
+      cookbook_file '/logs/foo/error.log' do
+        source 'error.log'
+        not_if { system('wget https://www.bar.com/foobar.txt -O /dev/null') }
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Don't use Ruby to shellout in a only_if / not_if conditional when you can just shellout directly by wrapping the command in quotes.
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      cookbook_file '/logs/foo/error.log' do
+        source 'error.log'
+        not_if 'wget https://www.bar.com/foobar.txt -O /dev/null'
+      end
+    RUBY
+  end
+
+  it 'registers an offense with a only_if that shells out using shell_out' do
+    expect_offense(<<~RUBY)
+      cookbook_file '/logs/foo/error.log' do
+        source 'error.log'
+        only_if { shell_out('wget https://www.bar.com/foobar.txt -O /dev/null').exitstatus == 0 }
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Don't use Ruby to shellout in a only_if / not_if conditional when you can just shellout directly by wrapping the command in quotes.
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      cookbook_file '/logs/foo/error.log' do
+        source 'error.log'
+        only_if 'wget https://www.bar.com/foobar.txt -O /dev/null'
+      end
+    RUBY
+  end
+
+  it 'does not register an offense if shell_out calls methods other than exitstatus == 0' do
+    expect_no_offenses(<<~RUBY)
+      template '/etc/foo' do
+        mode '0644'
+        source 'foo.erb'
+        only_if { shell_out('which docker').stdout.empty? }
+      end
+    RUBY
+  end
+
+  it 'does not register an offense if a conditional shells out directly using a string' do
+    expect_no_offenses(<<~RUBY)
+      template '/etc/foo' do
+        mode '0644'
+        source 'foo.erb'
+        only_if 'which docker'
+      end
+    RUBY
+  end
+end
