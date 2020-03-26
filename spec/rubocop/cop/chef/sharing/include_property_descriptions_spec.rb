15534dabb78a1c8fc4c911db110fd2829c1b5abc
Tim Smith <tsmith84@gmail.com>
2020-03-26 15:35:56 -0700

    Update specs for the new msg
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/rubocop/cop/chef/sharing/include_property_descriptions_spec.rb
+++ spec/rubocop/cop/chef/sharing/include_property_descriptions_spec.rb
@@ -23,14 +23,14 @@ describe RuboCop::Cop::Chef::ChefSharing::IncludePropertyDescriptions, :config d
   it 'registers an offense when a property does not include a description' do
     expect_offense(<<~RUBY)
     property :foo, String, name_property: true
-    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Resources should include description fields to allow automated documention. Requires Chef Infra Client 13.9 or later.
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Resource properties should include description fields to allow automated documention. Requires Chef Infra Client 13.9 or later.
     RUBY
   end
 
   it 'registers an offense when a property does not include a description or any other hash items' do
     expect_offense(<<~RUBY)
     property :foo, String
-    ^^^^^^^^^^^^^^^^^^^^^ Resources should include description fields to allow automated documention. Requires Chef Infra Client 13.9 or later.
+    ^^^^^^^^^^^^^^^^^^^^^ Resource properties should include description fields to allow automated documention. Requires Chef Infra Client 13.9 or later.
     RUBY
   end
 
