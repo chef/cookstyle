d609c65d86d7bffb368fd60e4a4d92c7b7a81bf7
Tim Smith <tsmith84@gmail.com>
2020-07-07 12:29:59 -0700

    Add ChefSharing/IncludeResourceExamples
    This is so we can fail our desktop cookbook builds if a PR doesn't includes examples. We're already doing this with resource and property description fields, which works great.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ lib/rubocop/cop/chef/sharing/include_resource_examples.rb
@@ -0,0 +1,59 @@
+#
+# Copyright:: Copyright 2020, Chef Software Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module RuboCop
+  module Cop
+    module Chef
+      module ChefSharing
+        # Resources should include examples field to allow automated documention. Requires Chef Infra Client 13.9 or later.
+        #
+        # @example
+        #
+        #   # good
+        #   examples <<~DOC
+        #     **Specify a global domain value**
+        #
+        #     ```ruby
+        #     macos_userdefaults 'full keyboard access to all controls' do
+        #       key 'AppleKeyboardUIMode'
+        #       value '2'
+        #     end
+        #     ```
+        #   DOC
+        #
+        class IncludeResourceExamples < Cop
+          include RangeHelp
+          extend TargetChefVersion
+
+          minimum_target_chef_version '13.9'
+
+          MSG = 'Resources should include examples field to allow automated documention. Requires Chef Infra Client 13.9 or later.'.freeze
+
+          def investigate(processed_source)
+            return if processed_source.blank?
+
+            # Using range similar to RuboCop::Cop::Naming::Filename (file_name.rb)
+            range = source_range(processed_source.buffer, 1, 0)
+
+            add_offense(nil, location: range, message: MSG, severity: :refactor) unless resource_examples(processed_source.ast).any?
+          end
+
+          def_node_search :resource_examples, '(send nil? :examples dstr ...)'
+        end
+      end
+    end
+  end
+end
