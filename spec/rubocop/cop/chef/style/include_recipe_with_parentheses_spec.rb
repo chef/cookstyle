969d0e932344e65db853bb445513ee31d1410ec2
Tim Smith <tsmith84@gmail.com>
2020-07-08 23:00:32 -0700

    Add new ChefStyle/IncludeRecipeWithParentheses
    Yes it's just a method, but none of our examples show it being called
    like a method and reading this code may confuse others later on. Let's
    set the style to be bare.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ spec/rubocop/cop/chef/style/include_recipe_with_parentheses_spec.rb
@@ -0,0 +1,37 @@
+#
+# Copyright:: 2020, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::ChefStyle::IncludeRecipeWithParentheses do
+  subject(:cop) { described_class.new }
+
+  it "registers an offense with include_recipe('foo')" do
+    expect_offense(<<~RUBY)
+      include_recipe('foo')
+      ^^^^^^^^^^^^^^^^^^^^^ There is no need to wrap the recipe in parentheses when using the include_recipe helper
+    RUBY
+
+    expect_correction(<<~RUBY)
+    include_recipe 'foo'
+    RUBY
+  end
+
+  it "does not register an with include_recipe 'foo'" do
+    expect_no_offenses("include_recipe 'foo'")
+  end
+end
