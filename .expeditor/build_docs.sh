ebf1e9fbdca86845f4aeb3f259f5d6306b13eabe
Tim Smith <tsmith84@gmail.com>
2021-02-02 19:52:59 -0800

    Build yaml docs after each PR merge
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- .expeditor/build_docs.sh
+++ .expeditor/build_docs.sh
@@ -8,8 +8,9 @@
 # merge to the repo. We live in the future.
 ############################################################################
 
-bundle install --jobs=7 --retry=3 --without debug
+bundle install --jobs=7 --retry=3 --without debug profiling
 bundle exec rake generate_cops_documentation
+bundle exec rake generate_cops_yml_documentation
 bundle exec rake update_readme_cop_count
 
 # Once Expeditor finishes executing this script, it will commit the changes and push
