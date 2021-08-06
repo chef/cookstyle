cb065bb4398bf1ddc303659b15c9e234017277be
Tim Smith <tsmith@chef.io>
2021-08-06 14:37:11 -0700

    Add a new cop for detecting malformed cookbook depends
    Catch these before they error in the client
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ spec/rubocop/cop/chef/correctness/metadata_malformed_depends_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+#
+# Copyright:: 2021, Chef Software, Inc.
+# Author:: Tim Smith (<tsmith@chef.io>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::Correctness::MetadataMalformedDepends, :config do
+  subject(:cop) { described_class.new(config) }
+
+  it 'registers an when passing a name and a constraint without a comma' do
+    expect_offense(<<~RUBY)
+      depends 'some_awesome_cookbook' '= 4.5.5'
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ metadata.rb cookbook dependencies and version constraints should be comma separated
+    RUBY
+
+    expect_correction(<<~RUBY)
+      depends 'some_awesome_cookbook', '= 4.5.5'
+    RUBY
+  end
+
+  it "doesn't register an offense when passing constraints properly" do
+    expect_no_offenses(<<~RUBY)
+      depends 'some_awesome_cookbook', '= 4.5.5'
+    RUBY
+  end
+end
