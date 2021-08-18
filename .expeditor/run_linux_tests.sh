6ef8d2b3fd287033fe03eb4e61e6d445badea554
Swati Keshari <skeshari@msystechnologies.com>
2021-08-18 17:17:26 +0530

    Replace deprecated --without flag with bundle config
    Signed-off-by: Swati Keshari <skeshari@msystechnologies.com>
    
    fix failure
    
    Signed-off-by: Swati Keshari <skeshari@msystechnologies.com>
    
    Removing pry-stack_explorer gem dependency
    
    Signed-off-by: Swati Keshari <skeshari@msystechnologies.com>
    
    Revert "Removing pry-stack_explorer gem dependency"
    
    This reverts commit ad4b18aa3c1ce2978786cc67f0094fb070801e4b.
    
    Removing pry-stack_explorer amd pry-beybug gem dependency
    
    Signed-off-by: Swati Keshari <skeshari@msystechnologies.com>
    
    Revert "Removing pry-stack_explorer amd pry-beybug gem dependency"
    
    This reverts commit 0aacba93ec403360af9dfaa6a7c9327ba0dec11d.

--- .expeditor/run_linux_tests.sh
+++ .expeditor/run_linux_tests.sh
@@ -10,7 +10,8 @@ export LANG=C.UTF-8 LANGUAGE=C.UTF-8
 echo "--- bundle install"
 
 bundle config --local path vendor/bundle
-bundle install --jobs=7 --retry=3 --without profiling debug
+bundle config set --local without profiling debug
+bundle install --jobs=7 --retry=3
 
 echo "+++ bundle exec task"
 bundle exec $@
