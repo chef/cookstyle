ec0487cdc61cbb29d3963f375f6ec00724458f48
nikhil2611 <nikhilgupta2102@gmail.com>
2024-12-02 16:20:23 +0530

    verifying hab package
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

---
+++ habitat/tests/test.pester.ps1
@@ -0,0 +1,33 @@
+param(
+    [Parameter()]
+    [string]$PackageIdentifier = $(throw "Usage: test.ps1 [test_pkg_ident] e.g. test.ps1 ci/user-windows-default/1.0.0/20190812103929")
+)
+
+$PackageVersion = $PackageIdentifier.split('/')[2]
+
+Describe "cookstyle" {
+    Context "cookstyle" {
+        It "is an executable" {
+            hab pkg exec $PackageIdentifier cookstyle.bat --version
+            $? | Should be $true
+        }
+
+        <#
+          At some point hab's argument parsing changed and it started interpreting the trailing `--version` as being
+          an argument passed to hab instead of an argument to the command passed to `hab pkg exec`.
+
+          Powershell 5.1 and 7 appear to differ in how they treat following arguments as well, such that these two
+          versions of the command fail in powershell 5.1 (which is currently what is running in the windows machines
+          in Buildkite) but pass in powershell 7 (which is currently what is running in a stock Windows 10 VM).
+
+          $the_version = (hab pkg exec $PackageIdentifier cookstyle.bat '--version' | Out-String).split(':')[1].Trim()
+          $the_version = (hab pkg exec $PackageIdentifier cookstyle.bat --version | Out-String).split(':')[1].Trim()
+
+          This version of the command passes in powershell 5.1 but fails in powershell 7.
+        #>
+        It "is the expected version" {
+            $the_version = (hab pkg exec $PackageIdentifier cookstyle.bat -- --version | Out-String).split(':')[1].Trim()
+            $the_version | Should be $PackageVersion
+        }
+    }
+}
