262ed02db07e8db4721fc9b75db5cb08d6f3a3c6
nikhil2611 <nikhilgupta2102@gmail.com>
2024-12-06 15:28:03 +0530

    adding echo
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- habitat/tests/test.sh
+++ habitat/tests/test.sh
@@ -17,9 +17,11 @@ error () {
 [[ -n "$pkg_ident" ]] || error 'no hab package identity provided'
 
 package_version=$(awk -F / '{print $3}' <<<"$pkg_ident")
+echo $package_version
 
 cd "${project_root}"
 
 echo "--- :mag_right: Testing ${pkg_ident} executables"
 actual_version=$(hab pkg exec "${pkg_ident}" cookstyle -v | sed -n 's/^Cookstyle \([0-9.]*\).*$/\1/p')
+echo $actual_version
 [[ "$package_version" = "$actual_version" ]] || error "cookstyle version is not the expected version. Expected '$package_version', got '$actual_version'"
