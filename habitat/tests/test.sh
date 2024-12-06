f38ede3f611ee47373acf2107d1b34ef8e35157c
nikhil2611 <nikhilgupta2102@gmail.com>
2024-12-06 15:13:42 +0530

    accepting arg pkg_idenent
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- habitat/tests/test.sh
+++ habitat/tests/test.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 
 project_root="$(git rev-parse --show-toplevel)"
+pkg_ident="$1"
 
 # print error message followed by usage and exit
 error () {
@@ -19,6 +20,6 @@ package_version=$(awk -F / '{print $3}' <<<"$pkg_ident")
 
 cd "${project_root}"
 
-echo "--- :msg_right: Testing ${pkg_ident} executables"
+echo "--- :mag_right: Testing ${pkg_ident} executables"
 actual_version=$(hab pkg exec "${pkg_ident}" cookstyle -v | sed -n 's/^Cookstyle \([0-9.]*\).*$/\1/p')
 [[ "$package_version" = "$actual_version" ]] || error "cookstyle version is not the expected version. Expected '$package_version', got '$actual_version'"
