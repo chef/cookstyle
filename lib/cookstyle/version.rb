c0e4b3d2a0b223abc49ac23d1a6c6208c2a7dc4f
nikhil2611 <nikhilgupta2102@gmail.com>
2025-01-22 17:08:58 +0530

    reverting the version back and removing the release branch changes
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- lib/cookstyle/version.rb
+++ lib/cookstyle/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 module Cookstyle
-  VERSION = "8.0.0" # rubocop: disable Style/StringLiterals
+  VERSION = "7.32.12" # rubocop: disable Style/StringLiterals
   RUBOCOP_VERSION = '1.25.1'
 end
