eb10db82da91d750e78638e20e46dade6b2a4768
Tim Smith <tsmith84@gmail.com>
2025-04-01 08:36:59 -0700

    Fix a few style issues on cookstyle itself
    Signed-off-by: Tim Smith <tsmith84@gmail.com>

--- lib/rubocop/monkey_patches/directive_comment.rb
+++ lib/rubocop/monkey_patches/directive_comment.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module RuboCop
-  # we're monkey patching the config regex to allow for # cookstyle: disable whatever
-  # in addition to the # rubocop: disable whatever that comes with RuboCop
+  # we're monkey patching the config regex to allow for "cookstyle: disable whatever"
+  # in addition to the "rubocop: disable whatever that comes with RuboCop"
   class DirectiveComment
     remove_const(:DIRECTIVE_COMMENT_REGEXP)
     DIRECTIVE_COMMENT_REGEXP = Regexp.new(
