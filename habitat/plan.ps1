9c48dc91249bf52593a6c1c91e26e1243a160ee3
nikhil2611 <nikhilgupta2102@gmail.com>
2025-07-16 13:25:22 +0530

    updated ruby 3.4 for cookstyle windows hab pkg
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = "Stop"
 $PSDefaultParameterValues['*:ErrorAction']='Stop'
 
-$env:HAB_BLDR_CHANNEL = "LTS-2024"
-$env:HAB_REFRESH_CHANNEL = "LTS-2024"
+$env:HAB_BLDR_CHANNEL = "base-2025"
+$env:HAB_REFRESH_CHANNEL = "base-2025"
 $pkg_name="cookstyle"
 $pkg_origin="chef"
 $parent_path = $($PLAN_CONTEXT | Split-Path -Parent)
@@ -10,7 +10,7 @@ $pkg_version=$(Get-Content "$parent_path/VERSION")
 $pkg_maintainer="The Chef Maintainers <humans@chef.io>"
 
 $pkg_deps=@(
-  "chef/ruby31-plus-devkit"
+  "core/ruby3_4-plus-devkit"
   "core/git"
 )
 $pkg_bin_dirs=@("bin"
