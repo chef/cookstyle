fc58d486973e14e6870e687b2cccc711ea9c3e69
LohithR22 <lohithrgowda22@gmail.com>
2025-07-26 15:10:18 +0530

    Add new cops for repeated resources in loops, and data bag load checks; added helper module for resource detection and used it in execute_apt_update and repeated_resource_in_loops

---
+++ lib/rubocop/cop/chef/performance/repeated_resources_in_loops.rb
@@ -0,0 +1,31 @@
+require_relative '../helpers/resource_matcher'
+
+module RuboCop
+  module Cop
+    module Chef
+      module Performance
+        class RepeatedResourcesInLoops < Base
+          include RuboCop::Cop::Chef::Helpers::ResourceMatcher
+
+          MSG = 'Avoid creating resources inside loops when the resource supports arrays: use batched or bulk resources for efficiency.'
+
+          def on_block(node)
+            return unless node.send_node.method_name == :each
+
+            # Get the body of the block, which could be a single node or a begin node (multiple statements)
+            body = node.body
+            statements = body.begin_type? ? body.children : [body]
+
+            statements.compact.each do |stmt|
+              next unless stmt.send_type?
+              method = stmt.method_name
+              if batchable_resource?(method) && stmt.receiver.nil?
+                add_offense(stmt.loc.expression, message: MSG)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
