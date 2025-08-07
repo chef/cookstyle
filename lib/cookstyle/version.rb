d921751709659beee3c919c37729a3491573df05
Chef Expeditor <chef-ci@chef.io>
2025-08-07 12:22:57 +0000

    Bump version to 8.4.0 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- lib/cookstyle/version.rb
+++ lib/cookstyle/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 module Cookstyle
-  VERSION = "8.3.1" # rubocop: disable Style/StringLiterals
+  VERSION = "8.4.0" # rubocop: disable Style/StringLiterals
   RUBOCOP_VERSION = '1.79.2'
 end
