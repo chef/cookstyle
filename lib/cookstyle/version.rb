652622a953bc1236ae275c8f8c00c6a6499fce2a
Tim Smith <tsmith84@proton.me>
2026-01-13 21:59:26 -0800

    Bump the version file as well
    Signed-off-by: Tim Smith <tsmith84@gmail.com>

--- lib/cookstyle/version.rb
+++ lib/cookstyle/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 module Cookstyle
-  VERSION = "8.5.5" # rubocop: disable Style/StringLiterals
+  VERSION = "8.6.0" # rubocop: disable Style/StringLiterals
   RUBOCOP_VERSION = '1.82.1'
 end
