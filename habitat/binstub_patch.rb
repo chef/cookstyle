ec56eb6cb5d1b9cea7602d0c9640fe837c21a034
nikhil2611 <ngupta@progress.com>
2026-06-01 17:31:43 +0530

    Fix cookstyle lint offenses in binstub_patch.rb
    Signed-off-by: nikhil2611 <ngupta@progress.com>

--- habitat/binstub_patch.rb
+++ habitat/binstub_patch.rb
@@ -1,4 +1,6 @@
-unless ENV["APPBUNDLER_ALLOW_RVM"]
-  ENV["APPBUNDLER_ALLOW_RVM"] = "true"
-  ENV["GEM_PATH"] = [File.expand_path(File.join(__dir__, "..", "vendor")), ENV["GEM_PATH"]].compact.join(File::PATH_SEPARATOR)
+# frozen_string_literal: true
+
+unless ENV['APPBUNDLER_ALLOW_RVM']
+  ENV['APPBUNDLER_ALLOW_RVM'] = 'true'
+  ENV['GEM_PATH'] = [File.expand_path(File.join(__dir__, '..', 'vendor')), ENV['GEM_PATH']].compact.join(File::PATH_SEPARATOR)
 end
