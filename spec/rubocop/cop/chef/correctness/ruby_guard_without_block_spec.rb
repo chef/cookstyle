148c6a5308add70bc858a8d2621e84cea7c9678a
Tim Smith <tsmith84@proton.me>
2026-08-14 04:33:48 -0700

    Add Chef/Correctness/RubyGuardWithoutBlock cop
    A not_if/only_if guard takes either a string, run as a shell command, or a
    block, run as Ruby. Passing a Ruby expression directly gives the guard the
    expression's result, because it is evaluated while the recipe compiles.
    
        not_if ::File.exist?('/etc/foo')
    
    A guard handed true or false raises at converge time with
    
        ArgumentError: Invalid only_if/not_if command, expected a string: true (TrueClass)
    
    and an expression that happens to return a string is accepted and then run
    as a shell command, so the guard quietly tests something else entirely.
    
    Only expressions that clearly produce a boolean are flagged: boolean
    literals, defined?, && and ||, comparison and negation operators, and
    predicate methods. A bare method call or a local variable is left alone
    since it may hold a perfectly good command string.
    
    Autocorrects by wrapping the expression in a block.
    
    This is the mistake reported in #841, where the cop message was reworded to
    make the block form clear. Nothing detected it.
    
    Signed-off-by: Tim Smith <tsmith84@proton.me>

---
+++ spec/rubocop/cop/chef/correctness/ruby_guard_without_block_spec.rb
@@ -0,0 +1,137 @@
+# frozen_string_literal: true
+#
+# Copyright:: Copyright (c) 2016-2025 Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
+# Author:: Tim Smith (<tsmith84@gmail.com>)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe RuboCop::Cop::Chef::Correctness::RubyGuardWithoutBlock, :config do
+  it 'registers an offense when not_if is passed a File.exist? call' do
+    expect_offense(<<~RUBY)
+      execute 'foo' do
+        not_if ::File.exist?('/etc/foo')
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ A Ruby expression used as a resource guard has to be wrapped in a block. Passing it directly hands the guard the expression result, which raises at converge time or silently runs a shell command.
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      execute 'foo' do
+        not_if { ::File.exist?('/etc/foo') }
+      end
+    RUBY
+  end
+
+  it 'registers an offense when only_if is passed a comparison' do
+    expect_offense(<<~RUBY)
+      execute 'foo' do
+        only_if node['foo']['version'] == '1.0'
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ A Ruby expression used as a resource guard has to be wrapped in a block. Passing it directly hands the guard the expression result, which raises at converge time or silently runs a shell command.
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      execute 'foo' do
+        only_if { node['foo']['version'] == '1.0' }
+      end
+    RUBY
+  end
+
+  it 'registers an offense when a guard is passed a negation' do
+    expect_offense(<<~RUBY)
+      execute 'foo' do
+        only_if !node['foo']['enabled']
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ A Ruby expression used as a resource guard has to be wrapped in a block. Passing it directly hands the guard the expression result, which raises at converge time or silently runs a shell command.
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      execute 'foo' do
+        only_if { !node['foo']['enabled'] }
+      end
+    RUBY
+  end
+
+  it 'registers an offense when a guard is passed a boolean literal' do
+    expect_offense(<<~RUBY)
+      execute 'foo' do
+        only_if true
+        ^^^^^^^^^^^^ A Ruby expression used as a resource guard has to be wrapped in a block. Passing it directly hands the guard the expression result, which raises at converge time or silently runs a shell command.
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      execute 'foo' do
+        only_if { true }
+      end
+    RUBY
+  end
+
+  it 'registers an offense when a guard is passed a compound expression' do
+    expect_offense(<<~RUBY)
+      execute 'foo' do
+        only_if ::File.exist?('/etc/foo') && node['foo']['enabled']
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ A Ruby expression used as a resource guard has to be wrapped in a block. Passing it directly hands the guard the expression result, which raises at converge time or silently runs a shell command.
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      execute 'foo' do
+        only_if { ::File.exist?('/etc/foo') && node['foo']['enabled'] }
+      end
+    RUBY
+  end
+
+  it "doesn't register an offense when the guard is a block" do
+    expect_no_offenses(<<~RUBY)
+      execute 'foo' do
+        not_if { ::File.exist?('/etc/foo') }
+      end
+    RUBY
+  end
+
+  it "doesn't register an offense when the guard is a shell command string" do
+    expect_no_offenses(<<~RUBY)
+      execute 'foo' do
+        not_if 'test -f /etc/foo'
+      end
+    RUBY
+  end
+
+  it "doesn't register an offense when the guard is an interpolated string" do
+    expect_no_offenses(<<~'RUBY')
+      execute 'foo' do
+        not_if "test -f #{node['foo']['path']}"
+      end
+    RUBY
+  end
+
+  it "doesn't register an offense when the guard is a variable holding a command" do
+    expect_no_offenses(<<~RUBY)
+      execute 'foo' do
+        not_if guard_command
+      end
+    RUBY
+  end
+
+  it "doesn't register an offense when the guard is a local variable" do
+    expect_no_offenses(<<~RUBY)
+      cmd = 'test -f /etc/foo'
+      execute 'foo' do
+        not_if cmd
+      end
+    RUBY
+  end
+end
