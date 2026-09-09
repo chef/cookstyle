64bc9ef15175bc52a1d96225c4e4887cc5150d59
Chef Expeditor <chef-ci@chef.io>
2026-09-09 06:33:37 +0000

    Bump version to 9.0.3 by Chef Expeditor
    Obvious fix; these changes are the result of automation not creative thinking.

--- lib/cookstyle/version.rb
+++ lib/cookstyle/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module Cookstyle
   # version of cookstyle
-  VERSION = "9.0.2" # rubocop: disable Style/StringLiterals
+  VERSION = "9.0.3" # rubocop: disable Style/StringLiterals
 
   # version of rubocop we pin to
   RUBOCOP_VERSION = '1.90.0'
